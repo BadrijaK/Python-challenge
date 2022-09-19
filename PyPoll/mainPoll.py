2bcb44a5082af404f3d44cba065f0c41a3e33fd0
BadrijaK <badrijak@gmail.com>
2022-09-19 18:32:47 +0100

    inserting git file

---
+++ PyPoll/mainPoll.py
@@ -0,0 +1,68 @@
+import os
+import csv
+
+filepath = os.path.join(".", "Resources", "election_data.csv")
+
+candidates = {}
+numberofVoters = 0
+candidatePercentages = {}
+highestnumberofVotes = 0
+
+with open(filepath) as csv_file:
+    csv_reader = csv.DictReader(csv_file, delimiter=",")
+    for voter in csv_reader:
+        numberofVoters = numberofVoters + 1
+        candidatevotedFor = voter["Candidate"]
+
+        try:
+            candidates[candidatevotedFor] = candidates[candidatevotedFor] + 1
+
+        except: 
+            candidates[candidatevotedFor] = 1
+for candidate in candidates:
+    numberofvotes = candidates[candidate]
+    percentage = (numberofvotes / numberofVoters) * 100
+    candidatePercentages[candidate] = percentage 
+
+    if numberofvotes > highestnumberofVotes:
+        highestnumberofVotes = numberofvotes 
+
+print("Election Results")
+print("--------------------")
+print(f"Total Votes: {numberofVoters}")
+print("--------------------")
+
+for candidate in candidates:
+    numberofvotes = candidates[candidate]
+    if numberofvotes == highestnumberofVotes:
+        winner = candidate
+
+
+    
+    
+    print(f"{candidate}: {percentage:.3f}% ({numberofvotes})")
+print("--------------------")
+print(f"Winner: {winner}")
+print("--------------------")
+
+writerpath = os.path.join(".", "Resources", "results.txt")
+
+with open(writerpath, "w") as text:
+    text.write("Election Result\n")
+    text.write("--------------------\n")
+    text.write(f"Total Votes: {numberofVoters}\n")
+    text.write("--------------------\n")
+
+    for candidate in candidates:
+        numberofvotes = candidates[candidate]
+        if numberofvotes == highestnumberofVotes:
+            winner = candidate
+
+
+        
+        
+        text.write(f"{candidate}: {percentage:.3f}% ({numberofvotes})\n")
+    text.write("--------------------\n")
+    text.write(f"Winner: {winner}\n")
+    text.write("--------------------\n")
+
---
+++ Pybank/main.py
@@ -0,0 +1,71 @@
+from cgitb import text
+from distutils import text_file
+import os
+import csv
+import math
+
+#Finding filepath of csv file
+filepath = os.path.join(".", "Resources", "budget_data.csv")
+
+
+months = []
+profits = []
+dates = []
+greatestincrease = -math.inf 
+greatestdecrease = math.inf
+
+#Openeing csv to be read
+with open(filepath) as csv_file:
+    csv_reader = csv.DictReader(csv_file, delimiter=",")
+
+#Looping through data 
+    for row in csv_reader:
+        months.append(row['Date'])
+        profits.append(int(row['Profit/Losses']))
+
+#Calculating length of all the months and the net total amount over entire period         
+totalmonth = len(months)
+totalprofit = sum(profits)
+
+#Looping through data to calculate the average of the changes in profits/losses
+totalProfitChanges = 0
+for i in range(1, len(profits)):
+    previousProfits = profits[i-1]
+    currentProfits = profits[i]
+    profitChange = currentProfits - previousProfits 
+    totalProfitChanges = totalProfitChanges + profitChange
+
+#Greatest increase and decrease from the profit changes    
+    if profitChange > greatestincrease:
+        greatestincrease = profitChange
+        max_date = max(months)
+    if profitChange < greatestdecrease:
+        greatestdecrease = profitChange
+        min_date = min(months)
+average = round(totalProfitChanges / (totalmonth -1),2)
+
+#Print final analysis report 
+print("Financial Analysis")
+print("----------------------")
+print(f"Total Months: {totalmonth}")
+print(f"Total: ${totalprofit}")
+print(f"Average change: ${average}")
+print(f'Greatest Increase in Profits: {max_date} ({greatestincrease})') 
+print(f'Greatest Increase in Profits: {min_date} ({greatestdecrease})')
+
+writerpath = os.path.join(".", "Resources", "results.txt")
+
+with open(writerpath, "w") as text:
+    text.write("Financial Analysis\n")
+    text.write("----------------------\n")
+    text.write(f"Total Months: {totalmonth}\n")
+    text.write(f"Total: ${totalprofit}\n")
+    text.write(f"Average change: ${average}\n")
+    text.write(f'Greatest Increase in Profits: {max_date} ({greatestincrease})\n') 
+    text.write(f'Greatest Increase in Profits: {min_date} ({greatestdecrease})\n')
+
+
+
+csv_file.close()
+
+
